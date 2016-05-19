c38d7603439f209ce3a332c6aaaee636db5ea1c3
cgudea <pulala@gmail.com>
2016-05-19 20:32:10 +0000

    Adds screen on for 10 seconds with motion Turns on screen on exit

--- screen_motion.py
+++ screen_motion.py
@@ -1,21 +1,39 @@
+#!/bin/usr/env python
 import RPi.GPIO as GPIO
 import time
+import os, sys
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 PIR_PIN = 7
 GPIO.setup(PIR_PIN, GPIO.IN)
 
 def MOTION(PIR_PIN):
-    print “Motion Detected!”
+    print "Motion Detected!"
+    with open("/sys/class/backlight/rpi_backlight/bl_power", 'w') as backlight:
+        backlight.write("0")
+        print "SCREEN ON SMARTIE"
 
-print “PIR Module Test (CTRL+C to exit)”
+    time.sleep(10)
+
+    with open("/sys/class/backlight/rpi_backlight/bl_power", 'w') as backlight:
+        backlight.write("1")
+        print "SCREEN OFF DUMMY"
+
+print "PIR Module Test (CTRL+C to exit)"
 time.sleep(2)
-print “Ready”
+print "Ready"
+with open("/sys/class/backlight/rpi_backlight/bl_power", 'w') as backlight:
+    backlight.write("1")
+    print "SCREEN OFF DUMMY"
 
 try:
     GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
     while 1:
         time.sleep(100)
 except KeyboardInterrupt:
-    print “ Quit”
+    print "Keyboard quit"
+finally:
     GPIO.cleanup()
+    with open("/sys/class/backlight/rpi_backlight/bl_power", 'w') as backlight:
+        backlight.write("0")
+        print "SCREEN ON SMARTIE"
